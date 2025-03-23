59dd2b67646d10d339290b70de0731d257d5bed7
elysianz <shawn.zhang@lincolnuni.ac.nz>
2025-03-23 13:28:49 +1300

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,4 +11,5 @@
 #
 
 # Add mtkiappd support for 802.11 k/v/r
-svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/mt/mtkiappd package/kernel/mt-drivers/mtkiappd
+#svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/mt/mtkiappd package/kernel/mt-drivers/mtkiappd
+svn export https://github.com/elysianz/actions-immortalwrt-mtk-sdk-phicomm-k2p/tree/main/files/package/kernel/mt-drivers/mtkiappd package/kernel/mt-drivers/mtkiappd
